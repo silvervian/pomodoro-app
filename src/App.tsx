440fd770b3471bb5f3278330d343c1f4f6d96589
Adrian Jedrczak <jedrczakadrian@gmail.com>
2023-03-23 22:43:57 +0100

    Add more settings to the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,23 @@ import "./App.css";
 
 import { gear, check, alarm } from "./assets";
 
-type Timer = {
-  minutes: number;
-  seconds: number;
-};
-
-const initialValues = { minutes: 1, seconds: 10 };
+// type Timer = {
+//   minutes: number;
+//   seconds: number;
+// };
 
 function App() {
   const [isRunning, setIsRunning] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
-  const [initialTimer, setInitialTimer] = useState<Timer>(initialValues);
-  const [timer, setTimer] = useState<Timer>(initialValues);
+  const [initialTimer, setInitialTimer] = useState<number>(90);
+  const [secondsLeft, setSecondsLeft] = useState<number>(90);
   const [progress, setProgress] = useState<number>(1596);
-  let timerRef = useRef<number | null>(null);
+  // let timerRef = useRef<number | null>(null);
 
-  useEffect(() => {
-    if (isRunning) {
-      document.title = `Pomodoro Timer | ${padNumber(timer.minutes)}:${padNumber(timer.seconds)}`;
-    }
-
-    return () => {
-      document.title = "Pomodoro Timer";
-      // if (timerRef.current !== null) {
-      //   window.clearInterval(timerRef.current);
-      // }
-    };
-  }, [isRunning]);
-
-  const changeSettings = () => {
-    setIsEditing(!isEditing);
-    stopTimer();
-  };
+  // const changeSettings = () => {
+  //   setIsEditing(!isEditing);
+  //   stopTimer();
+  // };
 
   const padNumber = (number: number): string | number => {
     if (!isEditing && number < 10) {
@@ -43,77 +28,55 @@ function App() {
     return number;
   };
 
-  const handleStartButton = () => {
-    if (!isRunning && !isEditing) {
-      startTimer();
-    } else if (isRunning) {
-      stopTimer();
-    }
-  };
+  // const handleStartButton = () => {
+  //   if (!isRunning && !isEditing) {
+  //     startTimer();
+  //   } else if (isRunning) {
+  //     stopTimer();
+  //   }
+  // };
 
   const tikTackToe = (totalSeconds: number) => {
     const tick = 1596 / totalSeconds;
     return tick;
   };
+  // setProgress((prev) => prev - tikTackToe(secondsLeft));
 
-  const startTimer = () => {
-    console.log("startTimer");
-    setIsRunning(true);
-    const totalSeconds = timer.minutes * 60 + timer.seconds;
-    const startTime = +new Date();
-    timerRef.current = window.setInterval(() => {
-      setProgress((prev) => prev - tikTackToe(totalSeconds));
-      const currentTime = +new Date();
-      const diff = currentTime - startTime;
-      const secondsLeft = totalSeconds - Math.floor(diff / 1000);
-      const minutesLeft = Math.floor(secondsLeft / 60);
-      setTimer({ minutes: minutesLeft, seconds: secondsLeft % 60 });
-      if (secondsLeft <= 0) {
-        finishTimer();
-      }
-    }, 1000);
-  };
-
-  const stopCountdown = () => {
-    if (timerRef.current) {
-      window.clearInterval(timerRef.current);
-      timerRef.current = null;
-    }
-  };
-
-  const stopTimer = () => {
-    setIsRunning(false);
-    stopCountdown();
-  };
-
-  const finishTimer = () => {
-    setIsRunning(false);
-    stopCountdown();
-    new Audio(alarm).play();
-    setTimer(initialTimer);
-  };
+  // const stopCountdown = () => {
+  //   if (timerRef.current) {
+  //     window.clearInterval(timerRef.current);
+  //     timerRef.current = null;
+  //   }
+  // };
 
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     const firstTwo = value.replace(/[^0-9]/, "").substring(0, 2);
-    setTimer({ ...timer, [name]: firstTwo });
-    setInitialTimer({ ...initialTimer, [name]: firstTwo });
+    // setTimer({ ...timer, [name]: firstTwo });
+    // setInitialTimer({ ...initialTimer, [name]: firstTwo });
   };
 
-  const { minutes, seconds } = timer;
+  const handleChangeSettings = () => {
+    setIsEditing(!isEditing);
+  };
 
   return (
-    <div className="App">
+    <div className="app">
+      <div className="labels">
+        <button className="option-btn active">pomodoro</button>
+        <button className="option-btn">short break</button>
+        <button className="option-btn">long break</button>
+      </div>
       <div className="wrapper">
         <div className="ring">
-          <svg width="518" height="518" viewBox="0 0 518 518">
+          <svg width="418" height="468" viewBox="0 0 418 418">
             <circle
               strokeWidth="10px"
               x="0"
               y="y"
-              cx="259"
-              cy="259"
-              r="254"
+              cx="159"
+              cy="159"
+              r="154"
               strokeLinecap="round"
               strokeDasharray="1596"
               strokeDashoffset={progress}
@@ -123,34 +86,19 @@ function App() {
 
         <div className="timer">
           <div className="time">
-            <div className="minutes">
-              <input
-                type="text"
-                name="minutes"
-                value={padNumber(minutes)}
-                onChange={handleChangeInput}
-                disabled={!isEditing}
-              />
-            </div>
+            <div className="minutes">40</div>
             <div className="colon">:</div>
-            <div className="seconds">
-              <input
-                type="text"
-                name="seconds"
-                value={padNumber(seconds)}
-                onChange={handleChangeInput}
-                disabled={!isEditing}
-              />
-            </div>
+            <div className="seconds">30</div>
           </div>
-          <button className="start" onClick={handleStartButton} disabled={isEditing}>
+          <button className="start" disabled={isEditing}>
             {isRunning ? "stop" : "start"}
           </button>
-          <button className="settings" onClick={changeSettings}>
-            <img src={isEditing ? check : gear} alt="Settings" />
+          <button className="settings">
+            <img src={gear} alt="Settings" onClick={handleChangeSettings} />
           </button>
         </div>
       </div>
+      <dialog>test</dialog>
     </div>
   );
 }
